95750941518a5df222a1b4235891a27606e1c160
Katapo13 <145489642+Katapo13@users.noreply.github.com>
2025-11-21 07:46:28 +0300

    feat: add user interface

---
+++ src/main/java/org/example/ArciverController.java
@@ -0,0 +1,219 @@
+package org.example;
+
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
+import lz78.LZ78_Compressor;
+import lz78.LZ78_Decompressor;
+import lzss.LZSS_Compressor;
+import lzss.LZSS_Decompressor;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ArciverController {
+
+    @FXML
+    private StackPane fileDropArea;
+    @FXML
+    private Label selectedFileLabel;
+    @FXML
+    private Label resultFileLabel;
+    @FXML
+    private RadioButton lz78RadioButton;
+    @FXML
+    private RadioButton lzssRadioButton;
+    @FXML
+    private ProgressBar progressBar;
+    @FXML
+    private Button saveButton;
+    @FXML
+    private HBox selectedFileBox;
+    @FXML
+    private HBox resultFileBox;
+
+
+    private File selectedFile;
+    private File resultFile;
+
+    @FXML
+    private void initialize() {
+        ToggleGroup compressionGroup = new ToggleGroup();
+        lz78RadioButton.setToggleGroup(compressionGroup);
+        lzssRadioButton.setToggleGroup(compressionGroup);
+        lz78RadioButton.setSelected(true);
+
+        fileDropArea.setOnMouseClicked(event -> {
+            chooseFile();
+        });
+
+    }
+
+    private void chooseFile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Выберите файл для архивации");
+        selectedFile = fileChooser.showOpenDialog(fileDropArea.getScene().getWindow());
+        if (selectedFile != null) {
+            selectedFileLabel.setText("Выбран файл: " + selectedFile.getName());
+            saveButton.setVisible(false);
+            resultFile = null;
+        }
+        selectedFileBox.setVisible(true);
+    }
+
+    @FXML
+    private void handleDragOver(javafx.scene.input.DragEvent event) {
+        if (event.getGestureSource() != fileDropArea && event.getDragboard().hasFiles()) {
+            event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+        }
+        event.consume();
+    }
+
+    @FXML
+    private void handleFileDrop(javafx.scene.input.DragEvent event) {
+        Dragboard db = event.getDragboard();
+        boolean success = false;
+        if (db.hasFiles()) {
+            selectedFile = db.getFiles().get(0);
+            selectedFileLabel.setText("Выбран файл: " + selectedFile.getName());
+            saveButton.setVisible(false);
+            resultFile = null;
+            success = true;
+        }
+        event.setDropCompleted(success);
+        event.consume();
+        selectedFileBox.setVisible(true);
+    }
+
+    @FXML
+    private void compressFile() {
+        if (selectedFile == null) {
+            showAlert("Файл не выбран", "Пожалуйста, выберите файл для архивации.");
+            return;
+        }
+        String algo = lz78RadioButton.isSelected() ? "LZ78" : "LZSS";
+        File outputFile = new File(selectedFile.getParent(), selectedFile.getName() + "." + algo.toLowerCase());
+//        FileChooser fileChooser = new FileChooser();
+//        fileChooser.setInitialFileName(selectedFile.getName() + "." + algo.toLowerCase());
+//        if (fileChooser.showSaveDialog(null) == null) {// пользователь отменил
+//            resultFile = outputFile;
+//        }else{
+//            resultFile = fileChooser.showSaveDialog(null);
+//        }
+        resultFile = outputFile;
+
+        Task<Void> task = new Task<>() {
+            @Override
+            protected Void call() throws Exception {
+//                updateProgress(0, 1);
+                  if (algo.equals("LZ78")) {
+                    new LZ78_Compressor().compress(selectedFile, outputFile);
+                  } else {
+                    new LZSS_Compressor().compress(selectedFile, outputFile);
+                  }
+//                updateProgress(1, 1);
+                return null;
+            }
+        };
+
+        runTask(task);
+    }
+
+    @FXML
+    private void decompressFile() {
+        if (selectedFile == null) {
+            showAlert("Файл не выбран", "Пожалуйста, выберите файл для разархивации.");
+            return;
+        }
+        String algo = lz78RadioButton.isSelected() ? "LZ78" : "LZSS";
+        String fileName = selectedFile.getName();
+        String originalFileName = fileName.substring(0, fileName.lastIndexOf('.'));
+        File outputFile = new File(selectedFile.getParent(), "uncompressed_" + originalFileName);
+        resultFile = outputFile;
+
+        Task<Void> task = new Task<>() {
+            @Override
+            protected Void call() throws Exception {
+                updateProgress(0, 1);
+                if (algo.equals("LZ78")) {
+                    new LZ78_Decompressor().decompress(selectedFile, outputFile);
+                } else {
+                    new LZSS_Decompressor().decompress(selectedFile, outputFile);
+                }
+                updateProgress(1, 1);
+                return null;
+            }
+        };
+        runTask(task);
+    }
+
+    private void runTask(Task<Void> task) {
+        progressBar.progressProperty().bind(task.progressProperty());
+        progressBar.setVisible(true);
+
+        task.setOnSucceeded(e -> {
+            progressBar.setVisible(false);
+            resultFileLabel.setText("Результат: " + resultFile.getName());
+            saveButton.setVisible(true);
+        });
+
+        task.setOnFailed(e -> {
+            resultFileBox.setVisible(false);
+            resultFileLabel.setText("");
+            resultFile = null;
+            progressBar.setVisible(false);
+            showAlert("Ошибка", "Произошла ошибка во время операции.");
+            e.getSource().getException().printStackTrace();
+        });
+
+        new Thread(task).start();
+        resultFileBox.setVisible(true);
+    }
+
+    @FXML
+    private void saveFile() {
+        if (resultFile != null && resultFile.exists()) {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setInitialFileName(resultFile.getName());
+            File dest = fileChooser.showSaveDialog(null);
+            if (dest != null) {
+                try {
+                    java.nio.file.Files.copy(resultFile.toPath(), dest.toPath(), java.nio.file.StandardCopyOption.REPLACE_EXISTING);
+                } catch (IOException e) {
+                    showAlert("Ошибка сохранения", "Не удалось сохранить файл.");
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void clearSelectedFile() {
+        selectedFile = null;
+        selectedFileLabel.setText("");
+        selectedFileBox.setVisible(false);
+        saveButton.setVisible(false);
+    }
+
+    @FXML
+    private void clearResultFile() {
+        resultFile = null;
+        resultFileLabel.setText("");
+        resultFileBox.setVisible(false);
+        saveButton.setVisible(false);
+    }
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,36 +1,72 @@
 package org.example;
+//import javafx.application.Application;
+//import javafx.geometry.Insets;
+//import javafx.geometry.Pos;
+//import javafx.scene.Scene;
+//import javafx.scene.control.*;
+//import javafx.scene.layout.*;
+//import javafx.stage.FileChooser;
+//import javafx.stage.Stage;
+//
 
-import lz78.LZ78_Compressor;
-import lz78.LZ78_Decompressor;
-import lzss.LZSS_Compressor;
-import lzss.LZSS_Decompressor;
-import java.io.File;
+//import lz78.LZ78_Compressor;
+//import lz78.LZ78_Decompressor;
+//import lzss.LZSS_Compressor;
+//import lzss.LZSS_Decompressor;
+//
+//import java.io.File;
+//import java.io.IOException;
+//
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        File input = new File("src/main/resources/Ducky.bmp");
+//        File compressed = new File("src/main/resources/output.lz78");
+//        File compressedSS = new File("src/main/resources/output.lzSS");
+//        File decompressed = new File("src/main/resources/decompressedDucky.bmp");
+//        File decompressedSS = new File("src/main/resources/decompressedSSDucky.bmp");
+//
+//        LZ78_Compressor compressor = new LZ78_Compressor();
+//        LZ78_Decompressor decompressor = new LZ78_Decompressor();
+//        LZSS_Compressor compressorSS = new LZSS_Compressor();
+//        LZSS_Decompressor decompressorSS = new LZSS_Decompressor();
+//
+//        try {
+//            compressor.compress(input, compressed);
+//            decompressor.decompress(compressed, decompressed);
+//
+//            compressorSS.compress(input, compressedSS);
+//            decompressorSS.decompress(compressedSS, decompressedSS);
+//        } catch (IOException ex) {
+//            System.out.println(ex.getMessage());
+//        }
+//    }
+//}
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.io.IOException;
+import java.util.Objects;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/org/example/archiver.fxml")));
+        Scene scene = new Scene(root, 800, 500);
+        primaryStage.setTitle("Архиватор файлов");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        File input = new File("src/main/resources/input.txt");
-        File compressed = new File("src/main/resources/output.lz78");
-        File compressedSS = new File("src/main/resources/output.lzSS");
-        File decompressed = new File("src/main/resources/decompressed.txt");
-        File decompressedSS = new File("src/main/resources/decompressedSS.txt");
-
-        LZ78_Compressor compressor = new LZ78_Compressor();
-        LZ78_Decompressor decompressor = new LZ78_Decompressor();
-        LZSS_Compressor compressorSS = new LZSS_Compressor();
-        LZSS_Decompressor decompressorSS = new LZSS_Decompressor();
-
-        try {
-            compressor.compress(input, compressed);
-            decompressor.decompress(compressed, decompressed);
-
-            compressorSS.compress(input, compressedSS);
-            decompressorSS.decompress(compressedSS, decompressedSS);
-        }
-        catch(IOException ex){
-            System.out.println(ex.getMessage());
-        }
+        launch(args);
     }
 }
+
+
